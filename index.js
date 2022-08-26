3e03193af70d56e8d8b051304d06eb6131d1cade
AshishKatlam <ashish.katlam@redbus.com>
2022-08-26 09:19:02 +0530

    [Fix1]

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 'use strict';
 
 const express = require('express');
-const { WebSocket } = require('ws');
+const { Server } = require('ws');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
-const WS_PORT  = 65080;
 
-
-var app = express();
-
-  // .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  // .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-const wss = new WebSocket.Server({port: WS_PORT},() => console.log(`WS Server is listening at ${WS_PORT}`));
+const server = express().use((req, res) =>res.sendFile(INDEX, { root: __dirname }))
+                        .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const wss = new Server({server},() => console.log(`WS Server is up`));
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
@@ -27,12 +22,3 @@ wss.on('connection', (ws) => {
   });
   ws.on('close', () => console.log('Client disconnected'));
 });
-
-app.get('/',(req, res) => res.sendFile(INDEX, {root: __dirname}));
-app.listen(PORT, () => console.log(`HTTP server listening at ${PORT}`));
-
-// setInterval(() => {
-//   wss.clients.forEach((client) => {
-//     client.send(new Date().toTimeString());
-//   });
-// }, 1000);
